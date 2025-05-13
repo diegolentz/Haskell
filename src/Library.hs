783e161711a31555cd9f637c68618a61b7a802c3
diegolentz <diegoolentz@gmail.com>
2025-05-12 22:23:41 -0300

    recursividad

--- src/Library.hs
+++ src/Library.hs
@@ -1,36 +1,34 @@
 module Library where
-import PdePreludat ( Eq((==)), Bool(..), fromInteger, (+), Number, even, (.), String, (>), (>=) )
+import PdePreludat ( Eq((==)), Bool(..), fromInteger, (+), Number, even, (.), String, (>), (>=), (*), (-), (||), Ord (max), error )
 
-lista :: [Number]
-lista = [1 .. 10]
+factorial :: Number -> Number
+factorial 0 = 1
+factorial n = n * factorial (n - 1)
 
-tupla :: (Number, Number)
-tupla = (1, 2)
+longitude :: [a] -> Number
+longitude [] = 0
+longitude (x:xs) = 1 + longitude xs
 
-head' :: [a] -> a
-head' (x : xs) = x
+ultimo :: [a] -> a
+ultimo [x] = x
+ultimo (x:xs) = ultimo xs
 
-tail' :: [a] -> [a]
-tail' (x : xs) = xs
+tomar :: Number -> [a] -> [a]
+tomar 0 xs = []
+tomar n [] = []
+tomar n (x:xs) | n > 0 = x : tomar (n - 1) xs
+
+esta :: Eq a => a -> [a] -> Bool
+esta elemento [] = False
+esta elemento (x:xs) = elemento == x || esta elemento xs
 
 
-fst' :: (a , b) -> a
-fst' (primero, segundo) = primero
+--En Haskell, la clase de tipo Ord se utiliza para tipos cuyos valores pueden ser comparados en términos de orden. Esto incluye operaciones como <, <=, >, >=, y también permite determinar el máximo o mínimo entre dos valores.
 
-snd' :: (a , b) -> b
-snd' (primero, segundo) = segundo
+maximo :: Ord a => [a] -> a
+maximo [x] = x
+maximo (x:y:ys) = maximo (x `max` y : ys)
 
 
---ejercicio alumno con sinonimos
-type Nombre = String
-type Nota = [Number]
-type Alumno = (Nombre, Nota)
 
-kevin :: Alumno
-kevin = ("Kevin", [10, 9, 8])
 
-nombre :: Alumno -> Nombre
-nombre = fst'
-
-notas :: Alumno -> Nota
-notas = snd'
