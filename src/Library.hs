fabbdfc2e3a2f3bc0020e0f7b9a2c502194eedf0
diegolentz <diegoolentz@gmail.com>
2025-05-13 17:29:29 -0300

    ejercicio de maderera

--- src/Library.hs
+++ src/Library.hs
@@ -1,26 +1,41 @@
 {-# OPTIONS_GHC -Wno-overlapping-patterns #-}
 module Library where
 
-import PdePreludat (Bool (..), Eq ((==)), Number, Ord (max), Show, String, error, even, fromInteger, (*), (+), (-), (.), (>), (>=), (||), reverse, otherwise, elem, and, or, sum, map, filter, flip)
+import PdePreludat (Bool (..), Eq ((==)), Number, Ord (max, (<=)), Show, String, error, even, fromInteger, (*), (+), (-), (.), (>), (>=), (||), reverse, otherwise, elem, and, or, sum, map, filter, flip, (/), not, (++))
 import Data.Type.Coercion (trans)
 
-data Ciudad = Ciudad
-  { nombre :: String,
-    temperaturaPromedio :: Number
-  }
-  deriving (Show)
+cuadruple :: Number -> Number
+cuadruple x = x * 4
 
-ciudades :: [Ciudad]
-ciudades =
-  [ Ciudad "Buenos Aires" 20,
-    Ciudad "CABA" 25,
-    Ciudad "La Plata" 30,
-    Ciudad "Rosario" 35,
-    Ciudad "Córdoba" 40
-  ]
+largoDeListon :: Number
+largoDeListon = 300
 
-hacemasDe :: Ciudad -> Number -> Bool
-hacemasDe ciudad temperatura  =  ( (> temperatura) .temperaturaPromedio  ) ciudad
+maderaCuadro :: Number -> Number
+maderaCuadro = cuadruple
 
-ciudadesCalurosas :: [Ciudad] -> [Ciudad]
-ciudadesCalurosas = filter (flip hacemasDe 30)
+meAlcanza :: Number -> Bool
+meAlcanza largo = largo <= largoDeListon
+
+cuantoMeSobra :: Number -> Number
+cuantoMeSobra cantidad = largoDeListon - cantidad
+
+quePorcentaje :: Number -> Number
+quePorcentaje cantidad = (cantidad / largoDeListon) * 100
+
+--func compuestas
+puedoHacerCuadroDe :: Number -> Bool
+puedoHacerCuadroDe lado = meAlcanza (maderaCuadro lado)
+
+puedoHacerCuadradoDe :: Number -> Bool
+puedoHacerCuadradoDe = meAlcanza . maderaCuadro
+
+-- ej 2
+
+esMayorEdad :: Number -> Bool
+esMayorEdad edad = edad >= 18
+
+esMenorEdad :: Number -> Bool
+esMenorEdad edad = not (esMayorEdad edad)
+
+nombreFormateado :: String -> String -> String
+nombreFormateado nombre apellido = apellido ++ " " ++ nombre
