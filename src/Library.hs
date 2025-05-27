98b4459dc4cf43fc52ca50bb3432dc12029148d2
diegolentz <diegoolentz@gmail.com>
2025-05-27 10:03:27 -0300

    ejercicio de maso de cartas

--- src/Library.hs
+++ src/Library.hs
@@ -1,4 +1,4 @@
-{-# OPTIONS_GHC -Wno-overlapping-patterns #-}
+-- {-# OPTIONS_GHC -Wno-overlapping-patterns #-}
 module Library where
 
 import PdePreludat (Bool (..), Eq ((==)), Number, Ord (max, (<=)), Show, String, error, even, fromInteger, (*), (+), (-), (.), (>), (>=), (||), reverse, otherwise, elem, and, or, sum, map, filter, flip, (/), not, (++), (&&), (^), any, take, length)
@@ -6,7 +6,7 @@ import Data.Type.Coercion (trans)
 import Data.Int (Int)
 import GHC.Num (Num)
 import Data.Char (toLower)
-
+import Number (Number)
 
 data Carta = Carta {
   nombre :: String,
@@ -15,49 +15,50 @@ data Carta = Carta {
   peso :: Number,
   fuerza :: Number,
   peleas :: Number,
-  tags :: [String] 
+  tags :: [String]
 } deriving (Show, Eq)
 
 maso :: [Carta]
 maso = [
-    Carta "Superman" 100 200 90 300 50 ["alguien", "kryptoniano"],
-    Carta "batman" 80 180 85 250 40 ["alguien", "terrestre"],
-    Carta "BatWonder Woman" 95 190 88 280 45 ["alien", "amazona"],
-    Carta "Flash" 120 170 70 200 60 ["alien", "terrestre"],
-    Carta "Aquaman" 85 185 92 260 35 ["alguien", "atlante"]
-    ]
+  Carta "batman" 10 10 10 10 10 ["humano", "vigilante"],
+  Carta "superman" 10 10 10 10 10 ["alguien", "super poderoso"],
+  Carta "flash" 10 10 10 10 10  ["humano", "velocista"],
+  Carta "linterna verde" 10 10 10 10 10 ["alguien", "vigilante"],
+  Carta "mujer maravilla" 10 10 10 10 10 ["humano", "avion invisible"],
+  Carta "aquaman" 10 10 10 10 10  ["humano", "vigilante del mar"],
+  Carta "Hulk" 10 10 10 10 10  ["alguien", "verde"]
+  ]
 
---obtener los nombres qe empiezan con bat
-obtenerEmpiezaConBat :: [Carta] -> [Carta]
-obtenerEmpiezaConBat [] = []
-obtenerEmpiezaConBat (carta : cartas)
-  | take 3 (map toLower (nombre carta)) == "bat" = carta : obtenerEmpiezaConBat cartas
-  | otherwise = obtenerEmpiezaConBat cartas
+largo :: Number
+largo = 10
 
--- averigar si hay cards con tags muy largos
-hayTagsLargos :: [Carta] -> [Carta]
-hayTagsLargos [] = []
-hayTagsLargos (carta:cartas)
-  | contieneTagLargo (tags carta) = carta : hayTagsLargos cartas
-  | otherwise = hayTagsLargos cartas
+-- comienzan con bat
+esBat :: Carta -> Bool
+esBat carta = take 3 (map toLower (nombre carta)) == "bat"
 
--- Función auxiliar para ver si una lista de tags tiene alguno largo
-contieneTagLargo :: [String] -> Bool
-contieneTagLargo [] = False
-contieneTagLargo (tag:resto)
-  | length tag > 10 = True
-  | otherwise = contieneTagLargo resto
+comienzaConBat :: [Carta] -> [Carta]
+comienzaConBat [] = []
+comienzaConBat (carta:cartas) 
+  | esBat carta = carta : comienzaConBat cartas
+  | otherwise   = comienzaConBat cartas
 
+-- tags muy largos
 
--- corrgir tag
--- Corrige las cartas reemplazando el tag "alguien" por "alien"
-corregirTag :: [Carta] -> [Carta]
-corregirTag [] = []
-corregirTag (carta:cartas) = carta { tags = corregirTags (tags carta) } : corregirTag cartas
+esTagLargo :: [String] -> Bool
+esTagLargo [] = False
+esTagLargo (tag:tags)
+  | length tag > largo = True
+  | otherwise          = esTagLargo tags 
 
--- Reemplaza "alguien" por "alien" en la lista de tags
-corregirTags :: [String] -> [String]
-corregirTags [] = []
-corregirTags (tag:resto)
-  | tag == "alguien" = "alien" : corregirTags resto
-  | otherwise        = tag : corregirTags resto
+tagMuyLargo :: [Carta] -> [Carta]
+tagMuyLargo [] = []
+tagMuyLargo (carta:cartas)
+  | esTagLargo (tags carta) = carta : tagMuyLargo cartas
+  | otherwise               = tagMuyLargo cartas
+
+-- cambiar alguien por alien
+cambiarAlguien :: [Carta] -> [Carta]
+cambiarAlguien [] = []
+cambiarAlguien (carta:cartas)
+  | elem "alguien" (tags carta) = carta {tags = ["alien"]} : cambiarAlguien cartas
+  | otherwise                   = cambiarAlguien cartas
